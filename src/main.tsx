9f46956a37d8e226f3ffdd46d62360a11841d723
musa gillani <musagillaniwork@gmail.com>
2023-01-13 15:29:31 +0500

    Added Home page Component

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Home from "./components/homepage";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
